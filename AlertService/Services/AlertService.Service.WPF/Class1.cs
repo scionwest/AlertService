bb87cfe1cc1b18aab27115b9aff1c776a66607ce
Johnathon <Johnathon@Sullinger.us>
2014-12-12 22:07:28 -0700

    Initail project check-ing. Not useable.

---
+++ AlertService/Services/AlertService.Service.WPF/Class1.cs
@@ -0,0 +1,22 @@
+﻿using AlertService.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlertService.Service.WPF
+{
+    public class AlertService : IAlertService
+    {
+        public Task ShowAlertAsync(string title, string content, params CallbackDelegate[] delegates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ShowAlertAsync(string title, object content, params CallbackDelegate[] delegates)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AlertService/Services/AlertService.Shared/IAlertService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlertService.Shared
+{
+    public interface IAlertService
+    {
+        Task ShowAlertAsync(string title, string content, params CallbackDelegate[] delegates);
+
+        Task ShowAlertAsync(string title, object content, params CallbackDelegate[] delegates);
+    }
+
+    public class CallbackDelegate
+    {
+        public string Name {get; private set;}
+
+        public Func<Task> Delegate {get; private set;}
+    }
+}
